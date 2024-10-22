a99daaee23a04ddd1c1ef4151cea3ba8b8412c37
Stellce <andrey.is323@gmail.com>
2024-10-22 15:49:45 +0200

    getFrames refactoring

--- dwn/getFrames.js
+++ dwn/getFrames.js
@@ -1,19 +1,35 @@
-const ffmpeg = require('ffmpeg');
+const extractFrames = require('ffmpeg-extract-frames');
+let [, , input, output] = process.argv;
 
-(function getFrames(file = "D:\\dwn\\test\\30sec.mp4", dir = "D:\\dwn\\test") {
-    try {
-        let process = ffmpeg(file);
-        process.then(video => {
-            video.fnExtractFrameToJPG(dir, {
-                every_n_frames: 1000
-            }, () => {
-                console.log("Finished");
-            }, err => {
-                console.log(err);
-            });
-        })
-    } catch (e) {
-        console.log(e);
-    }
-})()
-module.exports = {}
+validateArgs();
+
+const offsets = createOffsets();
+const extractOptions = {
+	input,
+	output,
+	offsets
+}
+
+console.log("Retrieving images...");
+
+extractFrames(extractOptions)
+	.then((res) => {
+		console.log("Images have been successfully retrieved", res);
+	}).catch(e => {
+		console.error(e);
+	});
+
+function validateArgs() {
+	if (!input) return console.log('Please, specify the folder of the video (first parameter)');
+	output = output || input + '_offsets_1';
+}
+
+function createOffsets() {
+	let offsets = [];
+	const min = 60_000;
+	const hour = 60*min;
+	const videoLength = 2*hour
+
+	for(let i = 5*min; i < videoLength; i+=min) offsets.push(i);
+	console.log(`Offsets: ${offsets}\n`);
+}
